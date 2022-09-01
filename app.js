611335335fc342c5037481a4002ef152d5eef456
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-08-31 17:04:24 -0700

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+
+
+
+
+console.log(hello-world);
+// declare a variable called x, assign it value of the value 10
+let x = 10;
+console.log(x);
+
+myName = 'Kassie';
+console.log(myName);
+
+const myName = 'Kassie';
+console.log(myName);
+
+console.log('Hello-World!');
+
+let message;
+let usersName = prompt('What is your name?')
+let message;
+console.log(usersName);
+message = 'Hi,' + usersName + '!';
+
+if (usersName == 'Kassie'){
+    message = 'Hiya Teach!';
+} else if (usersName == 'Ben'){message = "Hi Ben!";
+} else { message = 'Welcome to my site!'};
+
+document.write(message);
